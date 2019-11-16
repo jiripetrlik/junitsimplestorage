4e2f3a6140380dad63bcfe14f39f1038b2aa4a26
Jiri Petrlik <jiripetrlik@gmail.com>
2019-11-16 22:19:54 +0100

    Introduce connexion REST API

--- junit_simple_storage.py
+++ junit_simple_storage.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 
-from flask import Flask, escape, request
+import connexion
 
-app = Flask(__name__)
+app = connexion.App(__name__, specification_dir='./')
+app.add_api('swagger.yml')
 
 @app.route('/')
 def hello():
     return 'Hello junit simple storage'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
