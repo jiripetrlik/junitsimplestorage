4e45b7e07151345ede37e5805eaaf73d2efa9582
Jiri Petrlik <jiripetrlik@gmail.com>
2019-12-22 23:44:57 +0100

    Improve junit import

--- junitsimplestorage/junit.py
+++ junitsimplestorage/junit.py
@@ -1,26 +1,47 @@
-from junitparser import JUnitXml
+from junitparser import JUnitXml, TestSuite
 from junitsimplestorage.database import JunitTestRun
+import xml.etree.ElementTree as ET
 import datetime
 
 TEST_STATUS_PASSED = "passed"
 
 def loadJunitTestRuns(junitString):
-    xml = JUnitXml.fromstring(junitString)
+    testSuite = TestSuite.fromstring(junitString)
     testCasesList = []
-    for testSuite in xml:
-        for item in testSuite:
-            testRun = JunitTestRun(
-                testSuiteName = testSuite.name,
-                timestamp = datetime.datetime.strptime(testSuite.timestamp, '%Y-%m-%dT%H:%M:%S.%f'),
-                hostname = testSuite.hostname,
-                name = item.name,
-                classname = item.classname,
-                time = item.time,
-                state = testRunState(item),
-                message = testRunMessage(item)
-            )
-            
-            testCasesList.append(testRun)
+
+    root = ET.fromstring(junitString)
+    rootElementName = root.tag.lower()
+    if rootElementName == "testsuites":
+        testCasesList = []
+        xml = JUnitXml.fromelem(root)
+        for testSuite in xml:
+            testCasesList = testCasesList + importTestSuite(testSuite)
+
+        return testCasesList
+    elif rootElementName == "testsuite":
+        testSuite = TestSuite.fromelem(root)
+        return importTestSuite(testSuite)
+    else:
+        raise Exception("Unknown parent element for junit: " + rootElementName)
+
+def importTestSuite(testSuite):
+    testCasesList = []
+    for item in testSuite:
+        timestamp = testSuite.timestamp
+        if timestamp is not None:
+            timestamp = datetime.datetime.strptime(testSuite.timestamp, '%Y-%m-%dT%H:%M:%S.%f')
+
+        testRun = JunitTestRun(
+            testSuiteName = testSuite.name,
+            timestamp = timestamp,
+            hostname = testSuite.hostname,
+            name = item.name,
+            classname = item.classname,
+            time = item.time,
+            state = testRunState(item),
+            message = testRunMessage(item)
+        )    
+        testCasesList.append(testRun)
 
     return testCasesList
 
