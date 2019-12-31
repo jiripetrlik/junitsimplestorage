dbe40869415fd80becc3478ad5f449d87c9075ef
Jiri Petrlik <jiripetrlik@gmail.com>
2019-12-31 15:46:08 +0100

    Introduce setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+setup(
+    name="JunitSimpleStorage",
+    version="0.1",
+    packages=find_packages(),
+    author="Jiri Petrlik",
+    author_email="jiripetrlik@gmail.com",
+    install_requires=[
+        "connexion>=2.4.0",
+        "Flask>=1.1.1",
+        "Flask-SQLAlchemy>=2.4.1",
+        "junitparser>=1.4.0",
+        "PyMySQL>=0.9.3",
+        "PyYAML>=5.1.2",
+        "SQLAlchemy==1.3.10",
+        "swagger-ui-bundle==0.0.6"
+        ],
+)
